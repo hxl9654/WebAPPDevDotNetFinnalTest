db3cce6c59d085a1f3eae8dad8b4c66b9677619f
Xianglong He <admin@hxlxz.com>
2016-11-08 23:14:07 +0800

    完成数据表的Model

---
+++ WebSite/App_Code/DataTableModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Stock
+{
+    public int ID;
+    public int SellerID;
+    public int ModelID;
+    public int Quantity;
+}
+public class Model
+{
+    public int ModelID;
+    public int BrandID;
+    public string ModelName;
+    public string Color;
+    public string Picture;
+    public string Info;
+}
+public class Brand
+{
+    public int BrandID;
+    public string BrandName;
+    public string ProductAddress;
+}
+public class SellerUser
+{
+    public int UserID;
+    public int SellerID;
+    public string UserName;
+    public string UserPassWordHash;
+    public string Email;
+    public string Phone;
+}
+public class SellerInfo
+{
+    public int SellerID;
+    public string Phone;
+    public string Name;
+    public string Provience;
+    public string City;
+    public string Address;
+    public int Level;
+    public int ParentSellerID;
+}
+public class Admin
+{
+    public int UserID;
+    public string UserName;
+    public string UserPassWordHash;
+}
